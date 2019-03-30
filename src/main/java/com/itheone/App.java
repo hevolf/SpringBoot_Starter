9a7062f34c5cea54ce740f9afbfcd1410316d25b
hevolf <caohaifengx@163.com>
2019-03-30 20:08:12 +0800

    commit

---
+++ src/main/java/com/itheone/App.java
@@ -0,0 +1,10 @@
+package com.itheone;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caohaifengx@163.com 2019-03-14 16:47
+ */
+@SpringBootApplication
+public class App {
+}
